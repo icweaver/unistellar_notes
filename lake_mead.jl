37c53950558b23c0f9a70b6eddd21b44b30063e3
icweaver <iweaver@mailbox.org>
2022-11-08 20:11:57 -0800

    up

---
+++ lake_mead.jl
@@ -0,0 +1,58 @@
+### A Pluto.jl notebook ###
+# v0.19.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8f46e430-5fe4-11ed-2346-fb8fe7373ad5
+md"""
+Funding from parks: National Park Foundation
+  - school age students, urbanism, bus students in from city, LAKE program
+  - distance learning programming
+  - what are the cultural connections to astronomy?
+  - combining
+  - GBI (Great Basin Institute) grant possible assistant to work with Ian
+  - Aiming to reach 1,500 students, 60 teachers, thirty schools, 150 chaperones by May
+  - Program anticipates serving 1,000 students in Clark County School Districy, 500 students in Bullhead City School
+    District
+    - Transportation: 
+  - 4 distance learning products to be posted on the NPS site
+
+Astro in the area:
+  - Las Vegas Astronomical Society
+  - College of Southern Nevada: strong astronomy program
+  - Dr. John Keller RECON Program, rural telescope network program: www.tnoRECOn.net
+  - Ashley Pipkin: soundscapes and science
+  - Colors of Southern Nevada planetarium
+
+Western National Park $17,500 funding granted, could use to set up a wifi network with starlink
+
+Ask Franck about budget item for Ryan, NSPIRES, how does Ryan fit in?
+
+How many hours can I commit?
+
+National Park on-site possibility:
+
+There are flyers!
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═8f46e430-5fe4-11ed-2346-fb8fe7373ad5
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
